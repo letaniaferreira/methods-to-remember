afd29c9ff97080f8ea5c9c1dc60e91fe8d573c7e
Letania Ferreira <letaniaferreira@gmail.com>
2018-11-05 12:53:35 -0800

    Add more dict uses

--- zip_lists_to_make_dict.py
+++ zip_lists_to_make_dict.py
@@ -5,20 +5,20 @@
 # Put at least 3 key-value pairs in your dictionary.
 # Use a for loop to print out a series of statements such as "Willie is a dog."
 
-def animals(animals_name, animals_type):
-    """takes in tow list and creates a dictionary using zip"""
+# def animals(animals_name, animals_type):
+#     """takes in tow list and creates a dictionary using zip"""
 
-    animals_dict = {}
+#     animals_dict = {}
 
-    for name, kind in zip(animals_name, animals_type):
-        animals_dict[name] = kind
+#     for name, kind in zip(animals_name, animals_type):
+#         animals_dict[name] = kind
 
-    for kv in animals_dict.items():
-        print(kv[0] + ' is a ' + kv[1])
+#     for kv in animals_dict.items():
+#         print(kv[0] + ' is a ' + kv[1])
    
 
 
-animals(['ziggy', 'melo', 'dandi'], ['canary', 'dog', 'bear'])
+# animals(['ziggy', 'melo', 'dandi'], ['canary', 'dog', 'bear'])
 
 
 #another use for the zip method
@@ -26,9 +26,54 @@ def finder(arr1, arr2):
     arr1.sort()
     arr2.sort()
 
-    for num, n in zip(arr1, arr2):
-        if num != n:
-            return num
+    for n, m in zip(arr1,arr2):
+        if n != m:
+            return n
+
+
 
 result = finder([1, 2, 3, 4, 5, 6, 7], [3, 7, 2, 1, 4, 6])
 print(result)
+
+# #adding and removing for count
+# def finder(arr1, arr2):
+#     dic = {}
+#     for n in arr1:
+#         if n not in dic:
+#             dic[n] = 1
+#         else:
+#             dic[n] += 1
+
+#     for n in arr2:
+#         if n in dic:
+#             dic[n] -= 1
+
+#     for k, v in dic:
+#         if k != 0:
+#             return v
+
+
+# result = finder([1, 2, 3, 4, 5, 6, 7], [3, 7, 2, 1, 4, 6])
+# print(result)
+
+
+
+# #how to make a defaultdict
+
+# import collections
+
+# def finder(arr1, arr2):
+#     dicty = collections.defaultdict(int)
+#     for n in arr2:
+#         dicty[n] += 1
+
+#     for num in arr1:
+#         if dicty[num] == 0:
+#             return num
+#         else:
+#             dicty[num] -= 1
+
+
+
+# result = finder([1, 2, 3, 4, 5, 6, 7], [3, 7, 2, 1, 4, 6])
+# print(result)
