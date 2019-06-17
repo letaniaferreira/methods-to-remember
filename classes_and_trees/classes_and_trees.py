7dc50387bd9c39f753a4048aed62a469d40d6590
Letania Ferreira <letaniaferreira@gmail.com>
2019-06-17 06:52:49 -0700

    Add getter and setter to Node and is_empty and repr to Stack

--- classes_and_trees/classes_and_trees.py
+++ classes_and_trees/classes_and_trees.py
@@ -17,7 +17,12 @@ class Node():
         else:
             return False
 
-    
+    def get_left_node(self):
+        return self.left
+
+    def set_left_node(self, value):
+        self.left
+
 
     def __repr__(self):
         return self.value
@@ -37,13 +42,20 @@ class Stack():
     def pop(self):
         return self.items.pop()
 
+    def is_empty(self):
+        if len(self.items) == 0:
+            return True
+
+    def __repr__(self):
+        return self.items.__repr__()
+
 
 # class Tree():
-
+#
 #     def __init__(self):
 #         self.root = None
 #         self.items = Stack()
-
+#
 #     def has_left_node(self):
 #         if self.root == None:
 #             return False
