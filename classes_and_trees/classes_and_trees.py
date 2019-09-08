c7e1ead19eb5acc0d62f52e17620bc43e625f4e1
Letania Ferreira <letaniaferreira@gmail.com>
2019-09-07 23:44:39 -0700

    Add self

--- classes_and_trees/classes_and_trees.py
+++ classes_and_trees/classes_and_trees.py
@@ -20,14 +20,8 @@ class Node():
     def get_left_node(self):
         return self.left
 
-    def get_right_node(self):
-        return self.right
-
     def set_left_node(self, value):
-        self.left = value
-
-    def set_right_node(self, value):
-        self.right = value
+        self.left
 
 
     def __repr__(self):
@@ -48,37 +42,22 @@ class Stack():
     def pop(self):
         return self.items.pop()
 
-    def is_empty(self):
-        if len(self.items) == 0:
-            return True
-
     def __repr__(self):
+        # for item in self.items
         return self.items.__repr__()
 
 
 class Tree():
 
-    def __init__(self, value=None):
-        self.root = Node(value)
+    def __init__(self):
+        self.root = None
+        self.items = Stack()
 
-
-    def get_root(self):
-        return self.root
-
-
-def navigate_tree():
-    tree = Tree()
-    stack = Stack()
-    visited = []
-
-    node = tree.get_root()
-    stack.append(node)
-    if node.has_left_node():
-        stack.append(node.get_left_node())
-    if node.has_right_node():
-        stack.append(node.has_right_node())
-    visited.append(node)
-    stack.pop()
+    def has_left_node(self):
+        if self.root == None:
+            return False
+        node = self.root
+        return node.left != None
 
 
 
