436db059cf2588c6082834a5307d4e78118c0e73
Letania Ferreira <letaniaferreira@gmail.com>
2019-09-07 23:49:03 -0700

    Add diagonal difference

--- diagonal_matrix.py
+++ diagonal_matrix.py
@@ -12,3 +12,32 @@ diagonal_matrix([[1,  5,  9,  7],
               [ 6, 3,  5, -2],
               [ -1,  7,  7,  8],
               [7,  1, 3,  4]])
+
+
+#calculate left to right diagonal summ
+#calculate right to left diagonal summ
+#return different between them
+
+def diagonalDifference(arr):
+    # Write your code here
+    nrows = len(arr)
+    ncols = len(arr[0])
+
+    index = lcount = rcount = 0
+    #left to right
+    while index < nrows:
+        lcount += arr[index][index]
+        index += 1
+    index = 0
+    rindex = (nrows - 1)
+    while index < ncols:
+        rcount += arr[index] [rindex]
+        index += 1
+        rindex -= 1
+
+    if lcount > rcount:
+        return lcount - rcount
+    if lcount < rcount:
+        return rcount - lcount
+    else:
+        return 0
