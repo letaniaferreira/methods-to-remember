23888ed70589e470f7ee486f04372995d46cd68d
Letania Ferreira <letaniaferreira@gmail.com>
2020-07-19 11:33:48 -0700

    Add tests to account

--- ATM/test_account.py
+++ ATM/test_account.py
@@ -9,10 +9,15 @@ class TestAccount(TestCase):
         self.assertEqual(acc_1.balance, 0)
 
     def test_check_balance(self):
-        self.fail()
-
+        acc_1 = Account('Joe', 1234, 56578)
+        self.assertEqual(acc_1.check_balance(), acc_1.balance)
 
     def test_make_a_deposit(self):
         acc_1 = Account('Joe', 1234, 56578)
         acc_1.make_a_deposit(300)
-        self.assertEqual(acc_1.balance, 300)
+        self.assertEqual(acc_1.check_balance(), 300)
+
+    def test_make_a_withdrawal(self):
+        acc_1 = Account('Joe', 1234, 56578)
+        acc_1.make_a_withdrawal(50)
+        self.assertEqual(acc_1.check_balance(), -50)
