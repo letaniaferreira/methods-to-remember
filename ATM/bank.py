733d763b7592fcf9e889dab7d18f5d1bd44227c6
Letania Ferreira <letaniaferreira@gmail.com>
2020-07-19 22:06:29 -0700

    Add tests to create account

--- ATM/bank.py
+++ ATM/bank.py
@@ -1,10 +1,17 @@
-class bank:
+class Bank:
 
     def __init__(self):
         self.account = {}
 
     def create_account(self, account_number):
         if account_number not in self.account:
-            self.account['number'] = account_number
+            self.account[account_number] = account_number
+            return 'Account {} created'.format(account_number)
         else:
-            print('Account number already being used. Check records')
+            return 'Account number already being used. Check records'
+
+    # def get_account(self):
+    #     """Get account to make deposit or withdrawal"""
+    #
+    #
+    # def close_account(self):
--- ATM/test_bank.py
+++ ATM/test_bank.py
@@ -1,6 +1,14 @@
 from unittest import TestCase
 
+from ATM.bank import Bank
+
 
 class TestBank(TestCase):
     def test_create_account(self):
-        self.fail()
+        b1 = Bank()
+        self.assertEqual(b1.create_account(1234), 'Account 1234 created')
+
+    def test_cannot_create_duplicate_account(self):
+        b1 = Bank()
+        b1.create_account(1234)
+        self.assertEqual(b1.create_account(1234), 'Account number already being used. Check records')
