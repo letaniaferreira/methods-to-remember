18e7495da8e9545d45e79195e33bb7a282d4129f
Letania Ferreira <letaniaferreira@gmail.com>
2020-07-20 18:34:34 -0700

    Add get account

--- ATM/bank.py
+++ ATM/bank.py
@@ -4,14 +4,18 @@ class Bank:
         self.account = {}
 
     def create_account(self, account_number):
+        """Create an account"""
         if account_number not in self.account:
             self.account[account_number] = account_number
             return 'Account {} created'.format(account_number)
         else:
             return 'Account number already being used. Check records'
 
-    # def get_account(self):
-    #     """Get account to make deposit or withdrawal"""
-    #
+    def get_account(self, account_number):
+        """Get account to make deposit or withdrawal"""
+        if account_number in self.account:
+            return self.account[account_number]
+        else:
+            return 'Account {} not found'.format(account_number)
     #
     # def close_account(self):
--- ATM/test_bank.py
+++ ATM/test_bank.py
@@ -12,3 +12,12 @@ class TestBank(TestCase):
         b1 = Bank()
         b1.create_account(1234)
         self.assertEqual(b1.create_account(1234), 'Account number already being used. Check records')
+
+    def test_cannot_get_account(self):
+        b1 = Bank()
+        self.assertEqual(b1.get_account(2345), 'Account 2345 not found')
+
+    def test_get_account(self):
+        b1 = Bank()
+        b1.create_account(1234)
+        self.assertEqual(b1.get_account(1234), 1234)
